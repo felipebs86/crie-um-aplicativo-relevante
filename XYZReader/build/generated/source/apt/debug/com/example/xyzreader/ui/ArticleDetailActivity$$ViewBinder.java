e09b0792d8065feb77b2c8947d235153611f47d9
Felipe Bahiense <felipe.bahiense@b2wdigital.com>
2019-01-28 16:21:53 -0200

    ajustes pos review

--- XYZReader/build/generated/source/apt/debug/com/example/xyzreader/ui/ArticleDetailActivity$$ViewBinder.java
+++ XYZReader/build/generated/source/apt/debug/com/example/xyzreader/ui/ArticleDetailActivity$$ViewBinder.java
@@ -8,12 +8,12 @@ import butterknife.ButterKnife.ViewBinder;
 public class ArticleDetailActivity$$ViewBinder<T extends com.example.xyzreader.ui.ArticleDetailActivity> implements ViewBinder<T> {
   @Override public void bind(final Finder finder, final T target, Object source) {
     View view;
-    view = finder.findRequiredView(source, 2131558510, "field 'mUpButtonContainer'");
-    target.mUpButtonContainer = finder.castView(view, 2131558510, "field 'mUpButtonContainer'");
-    view = finder.findRequiredView(source, 2131558511, "field 'mUpButton'");
-    target.mUpButton = finder.castView(view, 2131558511, "field 'mUpButton'");
-    view = finder.findRequiredView(source, 2131558509, "field 'mPager'");
-    target.mPager = finder.castView(view, 2131558509, "field 'mPager'");
+    view = finder.findRequiredView(source, 2131230873, "field 'mUpButtonContainer'");
+    target.mUpButtonContainer = finder.castView(view, 2131230873, "field 'mUpButtonContainer'");
+    view = finder.findRequiredView(source, 2131230738, "field 'mUpButton'");
+    target.mUpButton = finder.castView(view, 2131230738, "field 'mUpButton'");
+    view = finder.findRequiredView(source, 2131230809, "field 'mPager'");
+    target.mPager = finder.castView(view, 2131230809, "field 'mPager'");
   }
 
   @Override public void unbind(T target) {
--- XYZReader/build/generated/source/apt/debug/com/example/xyzreader/ui/ArticleListActivity$$ViewBinder.java
+++ XYZReader/build/generated/source/apt/debug/com/example/xyzreader/ui/ArticleListActivity$$ViewBinder.java
@@ -8,12 +8,12 @@ import butterknife.ButterKnife.ViewBinder;
 public class ArticleListActivity$$ViewBinder<T extends com.example.xyzreader.ui.ArticleListActivity> implements ViewBinder<T> {
   @Override public void bind(final Finder finder, final T target, Object source) {
     View view;
-    view = finder.findRequiredView(source, 2131558513, "field 'mToolbar'");
-    target.mToolbar = finder.castView(view, 2131558513, "field 'mToolbar'");
-    view = finder.findRequiredView(source, 2131558514, "field 'mSwipeRefreshLayout'");
-    target.mSwipeRefreshLayout = finder.castView(view, 2131558514, "field 'mSwipeRefreshLayout'");
-    view = finder.findRequiredView(source, 2131558515, "field 'mRecyclerView'");
-    target.mRecyclerView = finder.castView(view, 2131558515, "field 'mRecyclerView'");
+    view = finder.findRequiredView(source, 2131230869, "field 'mToolbar'");
+    target.mToolbar = finder.castView(view, 2131230869, "field 'mToolbar'");
+    view = finder.findRequiredView(source, 2131230858, "field 'mSwipeRefreshLayout'");
+    target.mSwipeRefreshLayout = finder.castView(view, 2131230858, "field 'mSwipeRefreshLayout'");
+    view = finder.findRequiredView(source, 2131230818, "field 'mRecyclerView'");
+    target.mRecyclerView = finder.castView(view, 2131230818, "field 'mRecyclerView'");
   }
 
   @Override public void unbind(T target) {
